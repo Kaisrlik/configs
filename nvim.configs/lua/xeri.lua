fc2041b882d962819982013ae23edb7a231785ca
Jan Kaisrlik <ja.kaisrlik@gmail.com>
2026-03-10 14:30:08 +0100

    nvim: bump pacakges, fix pyhon3 provider

--- nvim.configs/lua/xeri.lua
+++ nvim.configs/lua/xeri.lua
@@ -6,3 +6,7 @@ require('xeri.treesitter')
 require('xeri.nvim-cmp')
 require('xeri.dap')
 require('xeri.telescope')
+
+-- Workaround to make sure provider is there: https://github.com/neovim/neovim/discussions/36368
+vim.g.loaded_python3_provider = nil
+vim.cmd('runtime autoload/provider/python3.vim')
