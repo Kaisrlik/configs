573dfa45bd171f9cd2dcbb75e46aa6d251cce72e
Jan Kaisrlik <ja.kaisrlik@gmail.com>
2026-06-03 14:41:29 +0200

    nvim: set fullpath for python3
    Python3 can be overwriten with nix packages, this patch forces to use
    system python3 which has all nvim requrements.

--- nvim.configs/lua/xeri.lua
+++ nvim.configs/lua/xeri.lua
@@ -1,6 +1,6 @@
 -- Workaround to make sure provider is there: https://github.com/neovim/neovim/discussions/36368
 vim.g.loaded_python3_provider = nil
-vim.g.python3_host_prog = 'python3'
+vim.g.python3_host_prog = '/run/current-system/sw/bin/python3'
 vim.cmd('runtime autoload/provider/python3.vim')
 
 require('xeri.nvim_cnf')
